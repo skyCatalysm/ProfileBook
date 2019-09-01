4156751251f74a848f944a5dbaa6b601902d802e
skyCatalysm <skycatalysm@gmail.com>
2019-09-01 18:51:45 +0800

    fixed the redirect on login. fixed porfolio pic

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -85,7 +85,7 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
             return new RedirectResponse($targetPath);
         }
 
-        return new RedirectResponse($this->urlGenerator->generate('index'));
+        return new RedirectResponse($this->urlGenerator->generate('portfolio'));
     }
 
     protected function getLoginUrl()
